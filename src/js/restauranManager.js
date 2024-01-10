2fbbcdb766135091237b58dc24716066567bd4b9
Cristina Gutierrez <cgutierrezestevez@gmail.com>
2024-01-10 12:03:55 +0100

    RestaurantManager avanzado con métodos
    Métodos avanzados y test comprobado hasta addDish

--- src/js/restauranManager.js
+++ src/js/restauranManager.js
@@ -1,7 +1,11 @@
 //Declaramos las clases específicas como clases
 class RestaurantsManagerException extends BaseException {
-  constructor(fileName, lineNumber) {
-    super("Error: RestaurantManager Exception.", fileName, lineNumber);
+  constructor(
+    message = "Error: RestaurantManager Generic Exception.",
+    fileName,
+    lineNumber
+  ) {
+    super(message, fileName, lineNumber);
     this.name = "RestaurantManager";
   }
 }
@@ -23,6 +27,12 @@ class CategoryException extends RestaurantsManagerException {
     this.name = "CategoryException";
   }
 }
+class MenuException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: The method needs a Menu parameter.", fileName, lineNumber);
+    this.name = "MenuException";
+  }
+}
 
 class AllergenException extends RestaurantsManagerException {
   constructor(fileName, lineNumber) {
@@ -35,32 +45,89 @@ class AllergenException extends RestaurantsManagerException {
   }
 }
 
-class MenuException extends RestaurantsManagerException {
+// class RestaurantException extends RestaurantsManagerException {
+//   constructor(fileName, lineNumber) {
+//     super(
+//       "Error: The method needs a Restaurant parameter.",
+//       fileName,
+//       lineNumber
+//     );
+//     this.name = "RestaurantException";
+//   }
+// }
+
+// class CoordinateException extends RestaurantsManagerException {
+//   constructor(fileName, lineNumber) {
+//     super(
+//       "Error: The method needs a Coordinate parameter.",
+//       fileName,
+//       lineNumber
+//     );
+//     this.name = "CoordinateException";
+//   }
+// }
+
+class CategoryInTheListException extends RestaurantsManagerException {
   constructor(fileName, lineNumber) {
-    super("Error: The method needs a Menu parameter.", fileName, lineNumber);
-    this.name = "MenuException";
+    super("Error: Category is already in the list.", fileName, lineNumber);
+    this.name = "CategoryInTheListException";
   }
 }
 
-class RestaurantException extends RestaurantsManagerException {
+class CategoryNotExistsInTheListException extends RestaurantsManagerException {
   constructor(fileName, lineNumber) {
     super(
-      "Error: The method needs a Restaurant parameter.",
+      "Error: The category doesn't exist in the list.",
       fileName,
       lineNumber
     );
-    this.name = "RestaurantException";
+    this.name = "CategoryNotExistsInTheListException";
   }
 }
 
-class CoordinateException extends RestaurantsManagerException {
+class MenuInTheListException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: Menu is already in the list.", fileName, lineNumber);
+    this.name = "MenuInTheListException";
+  }
+}
+
+class MenuNotExistsInTheListException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: The menu doesn't exist in the list.", fileName, lineNumber);
+    this.name = "MenuNotExistsInTheListException";
+  }
+}
+
+class AllergenInTheListException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: Allergen is already in the list.", fileName, lineNumber);
+    this.name = "AllergenInTheListException";
+  }
+}
+
+class AllergenNotExistsInTheListException extends RestaurantsManagerException {
   constructor(fileName, lineNumber) {
     super(
-      "Error: The method needs a Coordinate parameter.",
+      "Error: The allergen doesn't exist in the list.",
       fileName,
       lineNumber
     );
-    this.name = "CoordinateException";
+    this.name = "AllergenNotExistsInTheListException";
+  }
+}
+
+class DishInTheListException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: Dish is already in the list.", fileName, lineNumber);
+    this.name = "DishInTheListException";
+  }
+}
+
+class DishNotExistsInTheListException extends RestaurantsManagerException {
+  constructor(fileName, lineNumber) {
+    super("Error: The dish doesn't exist in the list.", fileName, lineNumber);
+    this.name = "DishNotExistsInTheListException";
   }
 }
 
@@ -68,77 +135,105 @@ class CoordinateException extends RestaurantsManagerException {
 let RestaurantsManager = (function () {
   let instanciated;
 
-  //Dentro de la función anónima declaramos la clase RestaurantsMager
-  class RestaurantsMager {
+  //Dentro de la función anónima declaramos la clase RestaurantsManager
+  class RestaurantsManager {
     //Definimos los atributos privados del objeto
-    #systemName = "";
-    #dishesCategory = {
-      category,
-      dishes: [],
-    };
-    #allergenType = {
-      alergen,
-      dishes: [],
-    };
-    #menus = {
-      menuName,
-      dishes: [],
-    };
-    #restaurants = {
-      restaurant,
-      coodenate,
-    };
-    constructor(systemName) {
+    #systemName = "Undefined";
+    #dishesCategory = [];
+    #allergenType = [];
+    #menus = [];
+    #restaurants = [];
+    #dishes = [];
+
+    //Declaración de métodos privados
+    //Dado una categoría, devuelve la posición de esa categoría o -1 si no lo encontramos
+    #getCategoryPosition(obj) {
+      return this.#dishesCategory.findIndex(
+        (busqueda) => busqueda.category === obj.category
+      );
+    }
+
+    //Dado un Menu, devuelve la posición de ese menú o -1 si no lo encontramos
+    #getMenuPosition(obj) {
+      return this.#menus.findIndex((busqueda) => busqueda.menu === obj.menu);
+    }
+
+    //Dado una alergia, devuelve la posición de esa alergoa o -1 so no lo encontramos
+    #getAllergenPosition(obj) {
+      return this.#allergenType.findIndex(
+        (busqueda) => busqueda.allerge === obj.allerge
+      );
+    }
+
+    //Dado un plato, devuelve la posición de esa plato o -1 so no lo encontramos
+    #getDishPosition(obj) {
+      return this.#dishes.findIndex((busqueda) => busqueda.dish === obj.dish);
+    }
+
+    constructor() {
       //La función se invoca con el operador new
       if (!new.target) throw new InvalidAccessConstructorException();
+
+      Object.defineProperty(this, "systemName", {
+        enumerable: true,
+        get() {
+          return this.#systemName;
+        },
+        set(value) {
+          if (!value) throw new EmptyValueException();
+          if (typeof value !== "string")
+            throw new InvalidValueException("systemName", value);
+          this.#systemName = value;
+        },
+      });
+
+      Object.defineProperty(this, "dishes", {
+        enumerable: true,
+        get() {
+          return this.#dishes;
+        },
+      });
     }
 
     //Definimos los métodos
-
     //Devuelve un iterador que permite recorrer las categorías del sistema
-    getterCategories() {
+    get categories() {
       const categories = this.#dishesCategory;
       return {
         *[Symbol.iterator]() {
-          for (const category in categories) {
-            if (categories.hasOwnProperty(category)) {
-              yield category;
-            }
+          for (const category of categories) {
+            yield category.category;
           }
         },
       };
     }
 
     //Devuelve un iterador que permite recorrer los menus del sistema
-    getterMenus() {
+    get menus() {
       const menuIte = this.#menus;
       return {
         *[Symbol.iterator]() {
           for (const menu of menuIte) {
-            if (menuIte.hasOwnProperty(menu)) {
-              yield menu;
-            }
+            yield menu.menu;
           }
         },
       };
     }
 
     //Devuelve un iterador que permite recorrer los alérgenos del sistema
-    getterAllergens() {
+    get allergens() {
       const allergens = this.#allergenType;
       return {
         *[Symbol.iterator]() {
           for (const allergen of allergens) {
-            if (allergens.hasOwnProperty(allergen)) {
-              yield allergen;
-            }
+            yield allergen.allerge;
           }
         },
       };
     }
 
     //Devuelve un iterador que permite recorrer los restaurantes del sistema
-    getterRestaurants() {
+    get restaurants() {
       const rest = this.#restaurants;
       return {
         *[Symbol.iterator]() {
@@ -150,10 +245,158 @@ let RestaurantsManager = (function () {
         },
       };
     }
+
+    //Añade una nueva categoría
+    addCategory(...categories) {
+      for (const cat of categories) {
+        if (!(cat instanceof Category)) {
+          throw new CategoryException();
+        }
+        if (cat === null) {
+          throw new EmptyValueException();
+        }
+        let obj = { category: cat, dishes: [] };
+        if (this.#getCategoryPosition(obj) !== -1) {
+          throw new CategoryInTheListException();
+        } else {
+          this.#dishesCategory.push(obj);
+          console.log("Successfully added");
+        }
+      }
+    }
+
+    //Elimina una categoría. Los platos quedarán desasignados de la categoria
+    removeCategory(...categories) {
+      for (const cat of categories) {
+        if (!(cat instanceof Category)) {
+          throw new CategoryException();
+        }
+        let obj = { category: cat, dishes: [] };
+        let position = this.#getCategoryPosition(obj);
+        if (position !== -1) {
+          this.#dishesCategory.splice(position, 1);
+          console.log("Succesfully removed");
+        } else {
+          throw new CategoryNotExistsInTheListException();
+        }
+      }
+    }
+
+    //Añade un nuevo menú
+    addMenu(...menus) {
+      for (const m of menus) {
+        if (!(m instanceof Menu)) {
+          throw new MenuException();
+        }
+        if (m === null) {
+          throw new EmptyValueException();
+        }
+        let obj = { menu: m, dishes: [] };
+        if (this.#getMenuPosition(obj) !== -1) {
+          throw new MenuInTheListException();
+        } else {
+          this.#menus.push(obj);
+          console.log("Successfully added");
+        }
+      }
+    }
+
+    //Elimina un menu
+    removeMenu(...menus) {
+      for (const m of menus) {
+        if (!(m instanceof Menu)) {
+          throw new MenuException();
+        }
+        let obj = { menu: m, dishes: [] };
+        let position = this.#getMenuPosition(obj);
+        if (position !== -1) {
+          this.#menus.splice(position, 1);
+          console.log("Succesfully removed");
+        } else {
+          throw new MenuNotExistsInTheListException();
+        }
+      }
+    }
+
+    //Añade un nuevo alérgeno
+    addAllergen(...allergens) {
+      for (const aller of allergens) {
+        if (!(aller instanceof Allergen)) {
+          throw new AllergenException();
+        }
+        if (aller === null) {
+          throw new EmptyValueException();
+        }
+        let obj = { allerge: aller, dishes: [] };
+        if (this.#getAllergenPosition(obj) !== -1) {
+          throw new AllergenInTheListException();
+        } else {
+          this.#allergenType.push(obj);
+          console.log("Successfully added");
+        }
+      }
+    }
+
+    //Elimina un alérgeno
+    removeAllergen(...allergens) {
+      for (const aller of allergens) {
+        if (!(aller instanceof Allergen)) {
+          throw new AllergenException();
+        }
+        let obj = { allerge: aller, dishes: [] };
+        let position = this.#getAllergenPosition(obj);
+        if (position !== -1) {
+          this.#allergenType.splice(position, 1);
+          console.log("Succesfully removed");
+        } else {
+          throw new AllergenNotExistsInTheListException();
+        }
+      }
+    }
+
+    //Añade un nuevo plato
+    addDish(...dishes) {
+      for (const di of dishes) {
+        if (!(di instanceof Dish)) {
+          throw new DishException();
+        }
+        if (di === null) {
+          throw new EmptyValueException();
+        }
+        let obj = { dish: di };
+        if (this.#getDishPosition(obj) !== -1) {
+          throw new DishInTheListException();
+        } else {
+          this.#dishes.push(obj);
+          console.log("Successfully added");
+        }
+      }
+    }
+
+    //Elimina un plato y todas sus asignaciones a categorias, alergenos y menus
+    // removeAllergen(...allergens) {
+    //   for (const aller of allergens) {
+    //     if (!(aller instanceof Allergen)) {
+    //       throw new AllergenException();
+    //     }
+    //     let obj = { allerge: aller, dishes: [] };
+    //     let position = this.#getAllergenPosition(obj);
+    //     if (position !== -1) {
+    //       this.#allergenType.splice(position, 1);
+    //       console.log("Succesfully removed");
+    //     } else {
+    //       throw new AllergenNotExistsInTheListException();
+    //     }
+    //   }
+    // }
   }
 
   //Inicialización del Singleton
-  function init() {}
+  function init() {
+    let rm = new RestaurantsManager();
+    Object.freeze(rm);
+    return rm;
+  }
   return {
     //Devuelve un objeto con el método getInstance
     getInstace: function () {
--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,24 @@
+//metemos en una función el for of para recorrer las categorías y que nos las muestre
+//eso nos evita ir repitiendo el mismo código
+function showCategories() {
+  console.log("List of categories");
+  for (const category of manager.categories) {
+    console.log(category);
+  }
+}
+function showMenus() {
+  console.log("List of menus");
+  for (const menu of manager.menus) {
+    console.log(menu);
+  }
+}
+function showAllergens() {
+  console.log("List of allergens");
+  for (const allergen of manager.allergens) {
+    console.log(allergen);
+  }
+}
+
 //Probando la creación de los objetos independientes
 console.log("Probando la creación de objetos independientes:");
 
@@ -8,6 +29,18 @@ dish01.description = "Pizza without pineapple, that is not pizza";
 dish01.ingredients = ["tomato", "chesse", "jam", "mushrooms"];
 dish01.image = "image";
 console.log(dish01.toString());
+let dish02 = new Dish("Hamburger");
+dish02.description = "The best hamburger that you eat in your life.";
+dish02.ingredients = [
+  "bread",
+  "meat of beef",
+  "letuce",
+  "tomato",
+  "bacon",
+  "onion",
+  "bbq sauce",
+];
+dish02.image = "image";
 
 //provocamos algunos fallos
 // let dish02 = new Dish();
@@ -20,6 +53,9 @@ let category01 = new Category("Vegetarian");
 console.log(category01.toString());
 category01.description = "This category is for vegetarian people";
 console.log(category01.toString());
+let category02 = new Category("Normal Category");
+category02.description =
+  "This is a normal category for people who do not have any type of allergen";
 
 //generamos algunos fallos
 // let category02 = new Category();
@@ -30,6 +66,8 @@ let allergen01 = new Allergen("Egg");
 console.log(allergen01.toString());
 allergen01.description = "Person who cannot eat egg";
 console.log(allergen01.toString());
+let allergen02 = new Allergen("Soya");
+allergen01.description = "Person who cannor ear soya";
 
 //generamos algunos fallos
 // let allergen02 = new Allergen();
@@ -39,8 +77,12 @@ console.log(allergen01.toString());
 //Probamos con el objeto Menu
 let menu01 = new Menu("Saturday Menu");
 console.log(menu01.toString());
-menu01.description = "Complete Menu with two Dishes, dessert and a drink.";
+menu01.description =
+  "Complete Menu with entrant, main dish, second dish, dessert and a drink.";
 console.log(menu01.description);
+let menu02 = new Menu("Daily Menu");
+menu02.description =
+  "Daily Menu with main dish, second dish, dessert and drink for a better price.";
 
 //generamos algunos fallos
 // let menu02 = new Menu();
@@ -65,3 +107,41 @@ console.log(restaurant01.toString());
 // let restaurant02 = new Restaurant();
 // restaurant01.description = 1236;
 // restaurant01.location = 110.3, 1523.6;
+
+//Probando RestaurantManager
+console.log("---------------Probando RestaurantManager---------------");
+
+let manager = RestaurantsManager.getInstace();
+manager.addCategory(category01);
+manager.addCategory(category02);
+// manager.addCategory(category01); //probamos a añadir de nuevo la misma categoría
+// mostramos las categorias de manager
+showCategories();
+manager.removeCategory(category01);
+// manager.removeCategory(category01); //probamos a eliminar la misma categoría
+// manager.removeCategory("hola que tal"); //probamos a eliminar algo que no sea una categoria
+// manager.removeCategory(restaurant01);
+// mostramos las categorias de manager.
+showCategories();
+
+manager.addMenu(menu01, menu02);
+// manager.addMenu(menu01); //probamos que no se pueda introducir un mismo menú
+showMenus();
+manager.removeMenu(menu01);
+// manager.removeMenu(menu01); //probamos a eliminar el mismo menu
+// manager.removeMenu(category01); //probamos a eliminar algo que no sea un menu
+showMenus();
+
+manager.addAllergen(allergen01, allergen02);
+// manager.addAllergen(allergen01); //probamos que no se pueda introducir un mismo menú
+// manager.addAllergen(menu01); //pronamos que solo se pueda introducir un alergeno
+showAllergens();
+manager.removeAllergen(allergen01);
+// manager.removeAllergen(allergen01); //probamos a eliminar el mismo alergeno
+// manager.removeAllergen(menu01); //probamo a eliminar un tipo que no sea Allergen
+showAllergens();
+
+manager.addDish(dish01, dish02);
+console.log(manager.dishes);
+// manager.addDish(menu01); //probamos a introducir un dato que no sea un plato
+// manager.addDish(dish01); //probamos a introducir un plato ya existente
